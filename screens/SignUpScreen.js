02c69edd0a2692da0974355dccd87dd83b88f2ab
Eryanto Agusriadi <eryantoagusriadi793@gmail.com>
2022-12-24 17:37:30 +0700

    Protecting Screens

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import { Alert } from "react-native";
 
+import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { AuthContext } from "../store/auth-context";
 import { createUser } from "../util/auth";
@@ -13,15 +14,15 @@ function SignupScreen() {
   async function signupHandler({ email, password }) {
     setIsAuthenticating(true);
     try {
-      await createUser(email, password);
-      authCtx.authenticate();
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
     } catch (error) {
       Alert.alert(
         "Authentication failed",
         "Could not create user, please check your input or try again later!"
       );
+      setIsAuthenticating(false);
     }
-    setIsAuthenticating(false);
   }
 
   if (isAuthenticating) {
